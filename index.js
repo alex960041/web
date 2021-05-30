2b31db3b09af68c818cd6c01257092f920caeda6
alex960041 <alex960041@gmail.com>
2021-05-30 21:12:11 +0800

    change image and html

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ var firstChar = name.slice(0,1);
 var upperCaseChar = firstChar.toUpperCase();
 var restOfName = name.slice(1,name.length);
 var CapitalizeName= upperCaseChar+restOfName;
-alert("hello, "+CapitalizeName+" welcome to my website");
+alert("hello, "+CapitalizeName+" welcome to our website");
